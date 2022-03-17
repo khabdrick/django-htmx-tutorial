a1b0ebd59942864a326198b1311b2456ab68c125
Muhammed Ali <muhamzyali@gmail.com>
2022-03-17 23:04:35 +0100

    update settings

--- django_htmx_tutorial/settings.py
+++ django_htmx_tutorial/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'django_htmx_tutorial.urls'
