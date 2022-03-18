8d9eb6175757f88b37189e19436f8b88ced15251
Muhammed Ali <muhamzyali@gmail.com>
2022-03-18 11:52:22 +0100

    remove django-htmx

--- django_htmx_tutorial/settings.py
+++ django_htmx_tutorial/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'django_htmx_tutorial.urls'
