e602cc04089a89dfb9e62f6d7eb23935a0dec637
Muhammed Ali <muhamzyali@gmail.com>
2022-03-18 12:23:16 +0100

    done witht the models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    phone_number=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
