58c51dd9ce37e59d2ccf06ea540f29616901cc3c
Muhammed Ali <muhamzyali@gmail.com>
2022-03-22 16:12:18 +0100

    clean up views.py file

--- app/views.py
+++ app/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
 from .models import Contact
 from django.views.generic.list import ListView
-from .forms import ContactForm
 
 def create_contact(request):
-    # form = ContactForm(request.POST)
-    # if request.method == "POST":
-    #     if form.is_valid():
-    #         form.save()
-
-    # contacts = Contact.objects.all()
-    # return render(request, 'contact-list.html', {'contacts': contacts})
 
     name = request.POST.get('contactname')
     phone_number = request.POST.get('phone_number')
     
     # add contact
     contact = Contact.objects.create(name=name, phone_number=phone_number)
-
-    
-    # add the contact to the contact list
-    # request.objects.add(contact)
-
-    # return template fragment with all the user's contacts
     contacts = Contact.objects.all()
     return render(request, 'contact-list.html', {'contacts': contacts})
 
