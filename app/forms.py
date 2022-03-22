9d00c7a677209cb0ad50c942ddca0d7cab0e720f
Muhammed Ali <muhamzyali@gmail.com>
2022-03-22 16:09:57 +0100

    update forms

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 class ContactForm(forms.ModelForm):
     class Meta:
-        fields = ["name", "tips"]
+        fields = ["name", "phone_number"]
         model = Contact
-        widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "phone_number": forms.TextInput(attrs={"class": "form-control"}),
-        }
+        # widgets = {
+        #     "name": forms.TextInput(attrs={"class": "form-control"}),
+        #     "phone_number": forms.TextInput(attrs={"class": "form-control"}),
+        # }
