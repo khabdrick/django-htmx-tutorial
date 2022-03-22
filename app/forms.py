21aa243cdadd30616f3004f8321890fab355cf6d
Muhammed Ali <muhamzyali@gmail.com>
2022-03-22 19:02:19 +0100

    clean up forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,3 @@ class ContactForm(forms.ModelForm):
     class Meta:
         fields = ["name", "phone_number"]
         model = Contact
-        # widgets = {
-        #     "name": forms.TextInput(attrs={"class": "form-control"}),
-        #     "phone_number": forms.TextInput(attrs={"class": "form-control"}),
-        # }
