0791fc68987565aa9b58f408ec5d648b832ca0ae
Muhammed Ali <muhamzyali@gmail.com>
2022-03-23 21:18:21 +0100

    update urls

--- django_htmx_tutorial/urls.py
+++ django_htmx_tutorial/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import create_contact, ContactList
+from app.views import create_contact, ContactList, delete_contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create-contact/', create_contact, name='create-contact'),
     path("contacts/", ContactList.as_view(), name='contact-list'),
+    path('delete-contact/<int:pk>/', delete_contact, name='delete-contact'),
+
 ]
